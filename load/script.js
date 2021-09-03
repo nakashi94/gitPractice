f909a46bbbd7b9a337e11ac1c4657aeda2fe8da7
nakashi94 <n0904yBUNJOU@gmail.com>
2021-09-04 01:17:57 +0900

    add load

---
+++ load/script.js
@@ -0,0 +1,9 @@
+function loaded() {
+    document.getElementById("loading").classList.remove("active");
+}
+
+window.addEventListener("load", function() {
+    setTimeout(loaded, 1000);
+})
+
+setTimeout(loaded, 5000);
